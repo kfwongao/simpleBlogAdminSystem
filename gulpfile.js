3ea3495f0cc9d7bee5988e1f4ff8f240dab4c0b5
Kin Fat WONG <kfwongao@gmail.com>
2018-12-05 21:31:45 +0800

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,27 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .styles([
+            'libs/blog-post.css',
+            'libs/bootstrap.css',
+            'libs/font-awesome.css',
+            'libs/metisMenu.css',
+            'libs/sb-admin-2.css',
+            'libs/styles.css'
+
+        ], './public/css/libs.css')
+
+        .scripts([
+            'libs/bootstrap.js',
+            'libs/jquery.js',
+            'libs/metisMenu.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+
+        ], './public/js/libs.js')
+
+    ;
+
+
 });
